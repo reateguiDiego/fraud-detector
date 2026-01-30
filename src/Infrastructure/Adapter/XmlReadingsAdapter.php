f0fe0d1f2bacd31ad8b7c4f1f47dba1e47008488
Diego <reategui.92@gmail.com>
2026-01-30 19:44:33 +0100

    feat: add xml readings loader adapter

---
+++ src/Infrastructure/Adapter/XmlReadingsAdapter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Infrastructure\Adapter;
+
+use App\Domain\Model\Reading;
+use App\Domain\Port\ReadingsLoaderInterface;
+
+class XmlReadingsAdapter implements ReadingsLoaderInterface
+{
+    public function supports(string $filePath): bool
+    {
+        return str_ends_with(strtolower($filePath), '.xml');
+    }
+
+    public function load(string $filePath): array
+    {
+        $xml = simplexml_load_file($filePath);
+        $readings = [];
+
+        foreach ($xml->reading as $item) {
+            $readings[] = new Reading((string)$item['client'], (string)$item['month'], (int)$item);
+        }
+
+        return $readings;
+    }
+}
